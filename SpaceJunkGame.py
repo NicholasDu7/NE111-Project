0ecd1b66e3ceb77f7c5f382cbe7a304ff5443fd4
Jeniton Augustinpillai <augustinjeniton@gmail.com>
2022-12-02 01:44:09 -0500

    Added Images
    - Added an image for the player
    - Added a coin image for the points
    - Added a background image
    - Fixed menu screen

--- SpaceJunkGame.py
+++ SpaceJunkGame.py
@@ -5,9 +5,10 @@ Date: 2022/11/01
 Group Members:
 Faisal Shahbaz (FS) @Faisal-Shahbaz (Github)
 Nicholas Du (ND)
-Jeniton Augustinpillai (JA)
+Jeniton Augustinpillai (JA) @JenitonA (GitHub)
 
 '''
+
 import random #import needed modules
 import os
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" %(20, 20) #positions the window on the screen. If run nativley, that 20x20 position refers to offset down and right from the top-left of your monitor
@@ -32,11 +33,22 @@ keySpace = False
 keyEscape = False
 keyH = False
 
+
 isAlive = True #FS game states
 cutscene = True
 currentScore = 0
 prevShoot = 0
 frenzy = False
+
+
+# JA These store player x and y locations
+playerX = 295
+playerY = 91
+
+# JA Image related variables
+playerImg = transform.rotate(transform.scale(image.load("spaceShip.png"), (40,40)), 90) # JA This variable will store image of spaceship
+backgroundImg = image.load("background.png") # JA Background image
+coinImg = transform.scale(image.load("coin.png"), (25,25)) # JA Coin image
 
 ship = Rect(300, 100, 20, 20) #defines the ship as type rect
 points = [] #include 4 rectangles
@@ -49,22 +61,26 @@ framerate = 60 #FS NEW framerate no longer constant for hard mode
 
 def drawCutscene(): #FS used in the first five seconds to generate the cutscene
     screen.blit(mainTitleFont.render('SPACE JUNK', 1, WHITE), Rect(350, 150, 50, 50)) #Title
-    screen.blit(scoreFont.render('Navigate the drone through the space junk debris field, and collect green', 1, WHITE), Rect(175, 250, 50, 50)) #FS Instructions
+    screen.blit(scoreFont.render('Navigate the drone through the space junk debris field, and collect coins', 1, WHITE), Rect(75, 250, 50, 50)) #FS Instructions
     screen.blit(scoreFont.render('Use WASD to navigate', 1, WHITE), Rect(350, 300, 50, 50)) #FS Instructions
-    screen.blit(scoreFont.render('Press SPACE to begin, press H for hard mode', 1, WHITE), Rect(200, 350, 50, 50)) #FS Instructions
+    screen.blit(scoreFont.render('Press SPACE to begin, press H for hard mode', 1, WHITE), Rect(220, 350, 50, 50)) #FS Instructions
     display.flip() #FS prints to display
 
+
+
 def drawScene(shipRect, junk, life, finalScore, points, lbullets, cooldown): #FS used to draw the screen. Given all elements that must be drawn
-    draw.rect(screen, BLACK, (0, 0, width, height)) #FS draws background
-    if cooldown==False: #FS NEW this changes the color of our ship based on whether it is on cooldown
-        draw.rect(screen, ORANGE, shipRect) #FS draws our ship
-    else:
-        draw.rect(screen, RED, shipRect) 
+    screen.blit(backgroundImg, (0,0)) #JA Draws background image 
+    screen.blit(playerImg, (playerX,playerY)) # JA draw player in correct location  
+    # if cooldown==False: #FS NEW this changes the color of our ship based on whether it is on cooldown
+    #     draw.rect(screen, ORANGE, shipRect) #FS draws our ship
+    # else:
+    #     draw.rect(screen, RED, shipRect) 
     for x in junk: #FS iterates through junk array to print each piece of junk
-        draw.rect(screen, BLUE, x)
+        draw.rect(screen, RED, x)
     for x in lbullets:
         draw.rect(screen, WHITE, x)
-    draw.rect(screen, GREEN, points[0])
+    screen.blit(coinImg, points[0])    
+    #draw.rect(screen, GREEN, points[0])
     if life == True: #FS prints score at top right
         screen.blit(scoreFont.render("%20s" %(finalScore), 1, WHITE), Rect(800, 20, 50, 50))
     if life == False: #FS prints death message
@@ -165,6 +181,7 @@ while running:
             if evnt.key == K_h: keyH = True
             if evnt.key == K_SPACE: keySpace = True
             if evnt.key == K_ESCAPE: keyEscape = True
+  
         #JA If the key is not pressed, then it will associate the specific key to False 
         if evnt.type == KEYUP: #FS checks if keys are no longer pressed
             if evnt.key == K_w: keyW = False
@@ -174,6 +191,7 @@ while running:
             if evnt.key == K_h: keyH = False
             if evnt.key == K_SPACE: keySpace = False
             if evnt.key == K_ESCAPE: keyEscape = False
+
         
     if cutscene == True: #FS for the cutscene
         drawCutscene() #JA Draws the cutscene at the beggining of the game
@@ -185,12 +203,16 @@ while running:
     if (isAlive == True) and (cutscene == False): #FS while playing the game
         if keyW == True: 
             ship[1] -= 5 #ND moves ship up (Note origin is top left of screen and axis is flipped)
+            playerY -=5
         if keyS == True:
             ship[1] += 5 #ND moves ship down
+            playerY +=5
         if keyA == True:
             ship[0] -= 5 #ND moves ship left
+            playerX -=5
         if keyD == True: #FS movement
             ship[0] += 5 #ND moves ship right
+            playerX +=5
         if keySpace == True and time.get_ticks() - prevShoot > 1500: #FS NEW this considers the cooldown for shooting bullets
             listBullets += genBullet(ship)
             prevShoot = time.get_ticks()
@@ -202,6 +224,8 @@ while running:
         (points, currentScore) = iterPoints(ship, points, frenzy, currentScore)
         drawScene(ship, listJunk, isAlive, currentScore, points, listBullets, time.get_ticks()-prevShoot>1500) #FS draws scene finally. It is passed every element that needs to be drawn 
     if (isAlive == False) and (keyEscape ==  True): running = False #FS if dead and exits
+
+        
     myClock.tick(framerate) #FS sets framerate. 
 
 quit() #JA Quits game at the end
